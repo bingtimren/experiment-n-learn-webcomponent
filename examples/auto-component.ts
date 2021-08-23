05372ccd9cce9390b984ca3ebc3b64f502db61ca
Bing Ren <bingtimren@gmail.com>
2021-08-23 11:01:22 +1000

    added link to external style in shadow dom

--- examples/auto-component.ts
+++ examples/auto-component.ts
@@ -6,10 +6,28 @@ export class AutoComponent extends HTMLElement{
         // Create a shadow root
         this.attachShadow({ mode: 'open' }); // sets and returns 'this.shadowRoot'
 
+
+        // apply external styles to the shadow dom
+        const linkElem = document.createElement('link');
+        linkElem.setAttribute('rel', 'stylesheet');
+        linkElem.setAttribute('href', '/examples/auto-component.css');      
+        this.shadowRoot!.append(linkElem);  
+
+        // get custom attribute
+        const customAttr = this.getAttribute("custom-attr");
+
         // Create (nested) span elements
         const wrapper = document.createElement('span');
-        const text = document.createTextNode('an autonomous custom element');
+        const text = document.createTextNode(`an autonomous custom element,`);
         wrapper.appendChild(text);
+
+        if (customAttr !== null) {
+            const attr = document.createElement("span");
+            attr.appendChild(document.createTextNode(`custom-attr="${customAttr}"`));
+            attr.setAttribute("class","auto-component red");
+            wrapper.appendChild(document.createTextNode(" "));
+            wrapper.appendChild(attr);            
+        }
 
         // attach element to shadow root
         this.shadowRoot!.append(wrapper);
